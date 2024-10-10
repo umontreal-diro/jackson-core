3d515d03e8f29b7f280e2617de1ab4df79583903
samuel <leumasmontreal@hotmail.com>
2024-10-10 14:35:48 -0400

    Continuation de documentation. Java Faker sprinkle.

--- src/test/java/com/fasterxml/jackson/core/sym/AddNameTest.java
+++ src/test/java/com/fasterxml/jackson/core/sym/AddNameTest.java
@@ -2,6 +2,7 @@ package com.fasterxml.jackson.core.sym;
 
 import com.fasterxml.jackson.core.JUnit5TestBase;
 import com.fasterxml.jackson.core.util.InternCache;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,10 +15,12 @@ public class AddNameTest extends JUnit5TestBase {
         final ByteQuadsCanonicalizer root = ByteQuadsCanonicalizer.createRoot(137);
         ByteQuadsCanonicalizer placeholder = root.makeChildOrPlaceholder(2);
 
-        String name = "Oceane";
+        Faker faker = new Faker();
+        String firstName = faker.name().firstName();
+
         int q1 = 2;
 
-        String result = placeholder.addName(name,q1);
+        String result = placeholder.addName(firstName,q1);
         assertEquals("Oceane",result);
     }
 
@@ -26,11 +29,12 @@ public class AddNameTest extends JUnit5TestBase {
         final ByteQuadsCanonicalizer root = ByteQuadsCanonicalizer.createRoot(137);
         ByteQuadsCanonicalizer placeholder = root.makeChildOrPlaceholder(2);
 
-        String name = "Oceane";
+        Faker faker = new Faker();
+        String lastName = faker.name().lastName();
         int q1 = 2;
         int q2 = 5;
 
-        String result = placeholder.addName(name,q1,q2);
+        String result = placeholder.addName(lastName,q1,q2);
         assertEquals("Oceane",result);
     }
 }
